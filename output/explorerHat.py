a43424b3026d02b79bbeb64e54c936cd8b5d8e7c
Alex Morley-Finch <alexmorleyfinch@gmail.com>
2017-04-02 22:16:19 +0100

    removed prints

--- output/explorerHat.py
+++ output/explorerHat.py
@@ -1,50 +1,36 @@
 #import explorerhat as eh
 
 def go():
-  output('go')
+  pass
   # eh.motor.one.backwards(100)
   # eh.motor.two.forwards(100)
 
 def reverse():
-  output('reverse')
+  pass
   # eh.motor.one.forwards(100)
   # eh.motor.two.backwards(100)
 
 def stop():
-  output('stop')
+  pass
   # eh.motor.one.stop()
   # eh.motor.two.stop()
 
 def left():
-  output('left')
+  pass
   # eh.motor.one.backwards(100)
   # eh.motor.two.backwards(100)
 
 def right():
-  output('right')
+  pass
   # eh.motor.one.forwards(100)
   # eh.motor.two.forwards(100)
 
 def leftBank():
-  output('leftBank')
+  pass
   # eh.motor.one.backward(70)
   # eh.motor.two.forwards(100)
 
 def rightBank():
-  output('rightBank')
+  pass
   # eh.motor.two.forwards(70)
   # eh.motor.one.backward(100)
-
-
-
-
-
-def output(s):
-  # used to mask long words
-  s += '                '
-  print(s, end='')
-  backspace(len(s))
-
-def backspace(n):
-  print('\r' * n, end='')
-
