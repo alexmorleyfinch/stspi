4702cadbaec78f7ba17f75d8f913295403addd84
Alex Morley-Finch <alexmorleyfinch@gmail.com>
2017-04-02 16:04:22 +0100

    minor change to motor controll terminal output

--- motorControl.py
+++ motorControl.py
@@ -1,36 +1,50 @@
 #import explorerhat as eh
 
 def go():
-  print('go')
+  output('go')
   # eh.motor.one.backwards(100)
   # eh.motor.two.forwards(100)
 
 def reverse():
-  print('reverse')
+  output('reverse')
   # eh.motor.one.forwards(100)
   # eh.motor.two.backwards(100)
 
 def stop():
-  print('stop')
+  output('stop')
   # eh.motor.one.stop()
   # eh.motor.two.stop()
 
 def left():
-  print('left')
+  output('left')
   # eh.motor.one.backwards(100)
   # eh.motor.two.backwards(100)
 
 def right():
-  print('right')
+  output('right')
   # eh.motor.one.forwards(100)
   # eh.motor.two.forwards(100)
 
 def leftBank():
-  print('leftBank')
+  output('leftBank')
   # eh.motor.one.backward(70)
   # eh.motor.two.forwards(100)
 
 def rightBank():
-  print('rightBank')
+  output('rightBank')
   # eh.motor.two.forwards(70)
   # eh.motor.one.backward(100)
+
+
+
+
+
+def output(s):
+  # used to mask long words
+  s += '                '
+  print(s, end='')
+  backspace(len(s))
+
+def backspace(n):
+  print('\r' * n, end='')
+
