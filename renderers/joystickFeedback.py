0e7af9d745485a0eff045bbdbc24e321fb22553b
Alex Morley-Finch <alexmorleyfinch@gmail.com>
2017-04-02 19:10:18 +0100

    slightly improved display

--- renderers/joystickFeedback.py
+++ renderers/joystickFeedback.py
@@ -1,15 +1,21 @@
 import pygame
 import constants
 
-def renderText(screen, string, x, y):
+positionMap = {
+  constants.EX: [300, 100],
+  constants.SQUARE: [250, 50],
+  constants.LEFT: [100, 50],
+  constants.RIGHT: [200, 50],
+}
+
+def renderText(screen, string, isGrey, key):
   font = pygame.font.Font(None, 20)
-  text_bitmap = font.render(string, True, constants.BLACK)
-  screen.blit(text_bitmap, [x, y])
+  text_bitmap = font.render(string, True, constants.GREY if not isGrey else constants.BLACK)
+  screen.blit(text_bitmap, [positionMap[key][0], positionMap[key][1]])
 
 
 def renderJoystickFeedback(screen, buttons):
   i = 1
   for key in buttons:
-    if not buttons[key]: continue
-    renderText(screen, constants.BUTTONS[key], 20, i*20)
+    renderText(screen, constants.BUTTONS[key], buttons[key], key)
     i=i+1
