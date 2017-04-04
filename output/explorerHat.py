64b9b39c06ba735e69e7a505dd2c1d1a5a147cd3
Alex Morley-Finch <alexmorleyfinch@gmail.com>
2017-04-04 22:07:08 +0000

    uncommented explorerHat

--- output/explorerHat.py
+++ output/explorerHat.py
@@ -1,36 +1,29 @@
-#import explorerhat as eh
+import explorerhat as eh
 
 def go():
-  pass
-  # eh.motor.one.backwards(100)
-  # eh.motor.two.forwards(100)
+  eh.motor.one.backwards(100)
+  eh.motor.two.forwards(100)
 
 def reverse():
-  pass
-  # eh.motor.one.forwards(100)
-  # eh.motor.two.backwards(100)
+  eh.motor.one.forwards(100)
+  eh.motor.two.backwards(100)
 
 def stop():
-  pass
-  # eh.motor.one.stop()
-  # eh.motor.two.stop()
+  eh.motor.one.stop()
+  eh.motor.two.stop()
 
 def left():
-  pass
-  # eh.motor.one.backwards(100)
-  # eh.motor.two.backwards(100)
+  eh.motor.one.forwards(100)
+  eh.motor.two.forwards(100)
 
 def right():
-  pass
-  # eh.motor.one.forwards(100)
-  # eh.motor.two.forwards(100)
+  eh.motor.one.backwards(100)
+  eh.motor.two.backwards(100)
 
 def leftBank():
-  pass
-  # eh.motor.one.backward(70)
-  # eh.motor.two.forwards(100)
+  eh.motor.one.backward(70)
+  eh.motor.two.forwards(100)
 
 def rightBank():
-  pass
-  # eh.motor.two.forwards(70)
-  # eh.motor.one.backward(100)
+  eh.motor.two.forwards(70)
+  eh.motor.one.backward(100)
